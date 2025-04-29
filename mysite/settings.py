ebcde98bf757b322c1379a8d62429e121d28a52d
JmmJmmJ <jy.maki@gmail.com>
2025-04-29 20:32:06 +0300

    Logging

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,15 +79,25 @@ SESSION_COOKIE_SAMESITE = None
 #    "disable_existing_loggers": False,
 #    "handlers": {
 #        "file": {
-#            "level": "DEBUG",
+#            "level": "INFO",
 #            "class": "logging.FileHandler",
-#            "filename": "debug.log",
+#            "filename": "info.log",
+#        },
+#        "fileError": {
+#            "level": "ERROR",
+#            "class": "logging.FileHandler",
+#            "filename": "error.log",
 #        },
 #    },
 #    "loggers": {
 #        "django": {
 #            "handlers": ["file"],
-#            "level": "DEBUG",
+#            "level": "INFO",
+#            "propagate": True,
+#       },
+#        "django2": {
+#            "handlers": ["fileError"],
+#            "level": "ERROR",
 #            "propagate": True,
 #        },
 #    },
