d913d4c10e808570b8fc3f9226d1398ddb92e3d3
JmmJmmJ <jy.maki@gmail.com>
2025-04-29 11:23:11 +0300

    Security Misconfiguration

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,6 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*9dia)$^5yzj5t7sn7fx-oltq2h-27e)ikvwf4ctmx6eh#(!m^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+#DEBUG = False
 DEBUG = True
 
 ALLOWED_HOSTS = []
