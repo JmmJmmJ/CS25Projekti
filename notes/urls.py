a0c5ee279c00843eaca2d52aa27a7b0cd5709a41
JmmJmmJ <jy.maki@gmail.com>
2025-04-30 11:08:34 +0300

    Update

--- notes/urls.py
+++ notes/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 
 from . import views
 
-#path("details/<int:id1>/", views.details, name="details"),
 urlpatterns = [
     path("", views.index, name="index"),
-    path("details/<str:id1>/", views.details, name="details"),
-    path('add/', views.addView, name='add'),
+    path('notes/<str:id>/add/', views.addView, name='add'),
+    path("notes/<str:id>/", views.notes, name="notes"),
+    #path("notes/", views.notes, name="notes"),
 ]
--- notes/views.py
+++ notes/views.py
@@ -5,15 +5,17 @@ from .models import Note
 
 @login_required
 def index(request):
-    notes = Note.objects.filter(owner=request.user)
-    return render(request, 'index.html', {'notes' : notes})
+    #return render(request, 'index.html')
+    return render(request, 'index.html', {'user' : request.user})
 
-def details(request, id1):
-    #note = Note.objects.get(id=id1)
-    query = 'SELECT * FROM notes_note WHERE id = %s' % id1
+#@login_required
+def notes(request, id):
+#def notes(request):
+    #notes = Note.objects.filter(owner=request.user)
+    query = 'SELECT * FROM notes_note WHERE owner_id = %s' % id
     notes = Note.objects.raw(query)
 
-    return render(request, 'details.html', {'notes' : notes})
+    return render(request, 'notes.html', {'notes' : notes})
 
 #@login_required
 #def details(request, id1):
@@ -24,7 +26,7 @@ def details(request, id1):
 #        return HttpResponseNotFound("<h1>Page not found</h1>")
 
 @login_required
-def addView(request):
+def addView(request, id):
 	if (request.method == 'POST'):
 		Note.objects.create(owner=request.user, note=request.POST.get('note'), description=request.POST.get('description'))
-	return redirect('/')
+	return redirect('/notes/' + id)
